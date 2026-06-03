4c0419102c55027b6ceccf6d5b10e20a3851acf1
Newwer:3 <136391325+Newwerrr@users.noreply.github.com>
2026-06-03 03:13:49 -0500

    make da thing

---
+++ AegisRunner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Aegis
+{
+    public class AegisRunner : MonoBehaviour
+    {
+        void Update()
+        {
+            AegisRuntime.ProcessQueue();
+            AegisRuntime.Update();
+        }
+
+        void LateUpdate()
+        {
+            AegisRuntime.Render();
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aegis
+{
+    public static class AegisRuntime
+    {
+        private static readonly Queue<Action> unityQueue = new();
+
+        private static bool initialized;
+
+        public static event Action OnRender;
+        public static event Action OnUpdate;
+
+        public static void Initialize()
+        {
+            if (initialized)
+                return;
+
+            var obj = new GameObject("Aegis");
+
+            UnityEngine.Object.DontDestroyOnLoad(obj);
+
+            obj.AddComponent<AegisRunner>();
+
+            initialized = true;
+        }
+
+        public static void RunOnUnityThread(Action action)
+        {
+            if (action == null)
+                return;
+
+            lock (unityQueue)
+            {
+                unityQueue.Enqueue(action);
+            }
+        }
+
+        internal static void ProcessQueue()
+        {
+            lock (unityQueue)
+            {
+                while (unityQueue.Count > 0)
+                {
+                    unityQueue.Dequeue()?.Invoke();
+                }
+            }
+        }
+
+        internal static void Update()
+        {
+            OnUpdate?.Invoke();
+        }
+
+        internal static void Render()
+        {
+            OnRender?.Invoke();
+        }
+    }
+}
